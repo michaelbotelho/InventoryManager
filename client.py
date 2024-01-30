ec91eb58d71561cdbf1b8ecbdf9d0084f3704f85
Michael Botelho <michaelbotelho51@gmail.com>
2024-01-30 00:37:26 -0500

    Modified client.py and server.py
    1. Completed implementation of gRPC functions in server.py.
    - Added semaphores
    - Added error handling
    - Added date and time logs
    2. Completed implementation of a client that connects to the server.
    3. Added docstring to project.

--- client.py
+++ client.py
@@ -8,40 +8,66 @@ import google.protobuf.empty_pb2
 
 
 def run(opcode):
+    """
+    Executes the corresponding gRPC request based on the given opcode.
+
+    Args:
+        opcode (int): The operation code representing the action to perform.
+    """
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = inventory_pb2_grpc.InventoryServiceStub(channel)
         try:
             if opcode == 0: # AddProduct Request
                 pid, pname, pquant, pprice = prompt(['id','name','quantity','price'])
+                print("Processing...")
                 response = stub.AddProduct(inventory_pb2.Product(product_identifier=pid, product_name=pname, product_quantity=pquant, product_price=pprice))
-                title = f"Recieved: Product successfully added {response.status}"
-                
+                title += f"Received: {response.status}"
+                    
             elif opcode == 1: # GetProductById Request
                 pid = prompt(['id'])
+                print("Processing...")
                 response = stub.GetProductById(inventory_pb2.ProductIdentifier(product_identifier=pid))
-                title = f"Recieved:\n\tProduct ID: {response.product_identifier} \n\tProduct Name: {response.product_name}\n\tProduct Quantity: x{response.product_quantity}\n\tProduct Price: ${response.product_price:.2f}"
+                if response.product_identifier == -1:
+                    title = f"Received: Product with ID {pid} does not exist."
+                elif response.product_identifier == -2:
+                    title = f"Received: Server failure."
+                else:
+                    title = f"Received:\n\tProduct ID: {response.product_identifier} \n\tProduct Name: {response.product_name}\n\tProduct Quantity: x{response.product_quantity}\n\tProduct Price: ${response.product_price:.2f}"
             
             elif opcode == 2: # UpdateProductQuantity Request
                 pid, pquant = prompt(['id', 'quantity'])
+                print("Processing...")
                 response = stub.UpdateProductQuantity(inventory_pb2.Quantity(product_identifier=pid, product_quantity=pquant))
-                title = f"Recieved: Product Quantity successfully updated {response.status}"
+                if response.product_identifier == -1:
+                    title = f"Received: Product with ID {pid} does not exist."
+                elif response.product_identifier == -2:
+                    title = f"Received: Server failure."
+                else:
+                    title = f"Received:\n\tProduct ID: {response.product_identifier} \n\tProduct Name: {response.product_name}\n\tUpdated Product Quantity: x{response.product_quantity}\n\tProduct Price: ${response.product_price:.2f}"
             
             elif opcode == 3: # DeleteProduct Request
                 pid = prompt(['id'])
+                print("Processing...")
                 response = stub.DeleteProduct(inventory_pb2.ProductIdentifier(product_identifier=pid))
-                pass
+                title = f"Received: {response.status}"
             
             elif opcode == 4: # GetAllProducts Request
+                print("Processing...")
                 response_iterator = stub.GetAllProducts(google.protobuf.empty_pb2.Empty())
                 for response in response_iterator:
-                    print(f"\nRecieved:\n\tProduct ID: {response.product_identifier} \n\tProduct Name: {response.product_name}\n\tProduct Quantity: x{response.product_quantity}\n\tProduct Price: ${response.product_price:.2f}")
+                    print(f"Received:\n\tProduct ID: {response.product_identifier} \n\tProduct Name: {response.product_name}\n\tProduct Quantity: x{response.product_quantity}\n\tProduct Price: ${response.product_price:.2f}")
 
-                if input("Continue using program? [Y/n]").lower() != 'y':
-                    pass
-                else:
-                    exit(0)
+                inp = input("Make another request? [Y/n]").lower()
+                while inp != 'y':
+                    if inp == 'n':
+                        system('cls')
+                        exit(0)
+                    inp = input("Make another request? [Y/n]").lower()
+                    
+                run(prompt())    
                 
             elif opcode == 5: # Exit program code
+                system('cls')
                 exit(0)
               
                 
@@ -53,20 +79,31 @@ def run(opcode):
             run(opcode) # Looping
             
         except TypeError:
-            if input("One or more inputs are of incorrect type. Continue? [Y/n]").lower() != 'y':
-                system('cls')
-                exit(0)
-            else:
-                run(prompt())
+            inp = input("One or more inputs are of incorrect type. Continue? [Y/n]").lower()
+            while inp != 'y':
+                if inp == 'n':
+                    system('cls')
+                    exit(0)
+                inp = input("One or more inputs are of incorrect type. Continue? [Y/n]").lower()
+                
+            run(prompt())
+            
         except KeyboardInterrupt:
             exit(0)
                 
 
         
-
-# Prompt user for types of information (including functionality selection)
-# Support for id, name, quantity, and price
 def prompt(type=0):
+    """
+    Prompts the user for information or action selection.
+    *Current support for action selection, id, name, quantity, and price
+
+    Args:
+        type (int or list): Determines the type of prompt. If 0, it prompts for action selection. If list, it prompts for specific information based on the list.
+
+    Returns:
+        int or list: The selected action or information.
+    """
     try:
         # Generating Selection menu
         if type == 0:
--- server.py
+++ server.py
@@ -1,54 +1,217 @@
-import grpc
 from concurrent import futures
+from datetime import datetime
+from threading import Semaphore
+
 import redis
+import grpc
 import inventory_pb2
 import inventory_pb2_grpc
 
+
+# Global lock (allows one thread access)
+LOCK = Semaphore(1)
+
 class InventoryServiceServicer(inventory_pb2_grpc.InventoryServiceServicer):
+    """
+    Implements the methods to handle inventory management operations.
+
+    Methods:
+        AddProduct: Adds a product to the inventory.
+        GetProductById: Retrieves a product from the inventory by ID.
+        UpdateProductQuantity: Updates the quantity of a product in the inventory.
+        DeleteProduct: Deletes a product from the inventory.
+        GetAllProducts: Retrieves all products from the inventory.
+    """
+    
     def AddProduct(self, request, context):
-        print("Request: Add Product\n", fr"{{'product_identifier : '{request.product_identifier}', 'product_name' : '{request.product_name}', 'product_quantity' : '{request.product_quantity}', 'product_price' : '{request.product_price: .2f}'}}")
-        r = redis.Redis(host='localhost', port=6379, decode_responses=True)
-        result = r.hset(request.product_identifier, mapping={
-                 'product_name': request.product_name,
-                 'product_quantity': request.product_quantity,
-                 'product_price': request.product_price
-                 })
-        return inventory_pb2.Status(status=True)
+        """
+        Adds a product to the inventory.
+
+        Args:
+            request (inventory_pb2.Product): The product to add.
+            context (grpc.ServicerContext): Context of the gRPC call.
+
+        Returns:
+            inventory_pb2.Status: Status of the operation.
+        """
+        if request.product_identifier < 0:
+            return inventory_pb2.Status(status="Cannot have a Product ID less than 0.")
+        try:
+            print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} Request: Add Product\n', fr"{{'product_identifier : '{request.product_identifier}', 'product_name' : '{request.product_name}', 'product_quantity' : '{request.product_quantity}', 'product_price' : '{request.product_price: .2f}'}}")
+            r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+        
+            # Return Status if product exists
+            if r.exists(request.product_identifier):
+                print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} Product exists, NOT added.')
+                return inventory_pb2.Status(status="Product already exists and was NOT added. \nTry deleting the product first, or change the Product ID.")
+            
+            # Add product and return status 
+            result = r.hset(request.product_identifier, mapping={
+                    'product_name': request.product_name,
+                    'product_quantity': request.product_quantity,
+                    'product_price': request.product_price
+                    })
+            
+            print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} Product added.')
+            return inventory_pb2.Status(status="Product successfully added.")
+            
+        except ConnectionError:
+            print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} Error connecting to redis-server. Maybe the server wasn\'t started?')
+            return inventory_pb2.Status(status="Server failure. Product was NOT added.")
         
         
     def GetProductById(self, request, context):
-        print(f"Request: Product ID {request.product_identifier}")
-        r = redis.Redis(host='localhost', port=6379, decode_responses=True)
-        result = r.hgetall(request.product_identifier)
-        print(f"Result: {result}")
-        return inventory_pb2.Product(product_identifier=int(request.product_identifier), product_name=str(result.get('product_name')), 
-                                     product_quantity=int(result.get('product_quantity')), product_price=float(result.get('product_price')))
-        
+        """
+        Retrieves a product from the inventory by ID.
+
+        Args:
+            request (inventory_pb2.Product): The product request containing the ID.
+            context (grpc.ServicerContext): Context of the gRPC call.
+
+        Returns:
+            inventory_pb2.Product: The retrieved product.
+        """
+        try:
+            print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} Request: Product ID {request.product_identifier}')
+            r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
+            LOCK.acquire()
+            # Return empty NULL product if product_identifier does not exist
+            if not r.exists(request.product_identifier):
+                print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} Product does not exist.')
+                return inventory_pb2.Product(product_identifier=-1, product_name="NULL", 
+                                            product_quantity=-1, product_price=float(-1))
+            
+            # Locate and return product
+            result = r.hgetall(request.product_identifier)
+            LOCK.release()
+            
+            print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} Product found.')
+            return inventory_pb2.Product(product_identifier=int(request.product_identifier), product_name=str(result.get('product_name')), 
+                                        product_quantity=int(result.get('product_quantity')), product_price=float(result.get('product_price')))
+           
+        except ConnectionError:
+            print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} Error connecting to redis-server. Maybe the server wasn\'t started?')
+            return inventory_pb2.Product(product_identifier=-2, product_name="NULL", 
+                                            product_quantity=-1, product_price=float(-1))
+            
         
     def UpdateProductQuantity(self, request, context):
-        print(f"Request: Update Product Quantity \n\tProduct ID: {request.product_identifier} \n\tQuantity: {request.product_quantity}")
-        r = redis.Redis(host='localhost', port=6379, decode_responses=True)
-        result = r.hset(request.product_identifier, 'product_quantity', request.product_quantity)
-        print(f"Result: {result}")
+        """
+        Updates the quantity of a product in the inventory.
+
+        Args:
+            request (inventory_pb2.Product): The product request containing the ID and new quantity.
+            context (grpc.ServicerContext): Context of the gRPC call.
+
+        Returns:
+            inventory_pb2.Product: The updated product.
+        """
+        try:
+            print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} Request: Update Product Quantity \n\tProduct ID: {request.product_identifier} \n\tQuantity: {request.product_quantity}')
+            r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+            
+            LOCK.acquire()
+            # Return empty NULL product if product_identifier does not exist
+            if not r.exists(request.product_identifier):
+                print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} Product does not exist.')
+                return inventory_pb2.Product(product_identifier=-1, product_name="NULL", 
+                                            product_quantity=-1, product_price=float(-1))
+                
+            # Locate, update quantity, and return product
+            result = r.hset(request.product_identifier, 'product_quantity', request.product_quantity)
+            LOCK.release()
+            
+            print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} Product found, quantity updated.')
+            return inventory_pb2.Product(product_identifier=int(request.product_identifier), product_name=str(result.get('product_name')), 
+                                        product_quantity=int(result.get('product_quantity')), product_price=float(result.get('product_price')))
+            
+        except ConnectionError:
+            print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} Error connecting to redis-server. Maybe the server wasn\'t started?')
+            return inventory_pb2.Product(product_identifier=-2, product_name="NULL", 
+                                            product_quantity=-1, product_price=float(-1))
     
     
     def DeleteProduct(self, request, context):
-        print(f"Request: Delete Product ID {request.product_identifier}")
-        pass
+        """
+        Deletes a product from the inventory.
+
+        Args:
+            request (inventory_pb2.Product): The product request containing the ID.
+            context (grpc.ServicerContext): Context of the gRPC call.
+
+        Returns:
+            inventory_pb2.Status: Status of the operation.
+        """
+        try:
+            print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} Request: Delete Product ID {request.product_identifier}')
+            print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} Request: Add Product\n', fr"{{'product_identifier : '{request.product_identifier}', 'product_name' : '{request.product_name}', 'product_quantity' : '{request.product_quantity}', 'product_price' : '{request.product_price: .2f}'}}")
+            r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+        
+            LOCK.acquire()
+            # Return Status if product does not exists
+            if not r.exists(request.product_identifier):
+                print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} Product does not exist, NOT deleted.')
+                return inventory_pb2.Status(status="Product does not exist and was NOT deleted.")
+            
+            # Delete product and return status 
+            result = r.delete(request.product_identifier)
+            LOCK.release()
+            
+            print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} Product deleted.')
+            return inventory_pb2.Status(status="Product successfully deleted.")
+        
+        except ConnectionError:
+            print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} Error connecting to redis-server. Maybe the server wasn\'t started?')
+            return inventory_pb2.Status(status="Server failure. Product was NOT deleted.")
+        
     
     def GetAllProducts(self, request, context):
-        print(f"Request: All Products")
-        r = redis.Redis(host='localhost', port=6379, decode_responses=True)
-        products = r.keys('*') # Return list of keys that match wildcard (return all keys)
+        """
+        Retrieves all products from the inventory.
+
+        Args:
+            request: Empty message.
+            context (grpc.ServicerContext): Context of the gRPC call.
+
+        Yields:
+            inventory_pb2.Product: The products in the inventory.
         
-        for i in range(len(products)):
-            result = r.hgetall(products[i])
-            yield inventory_pb2.Product(product_identifier=int(products[i]), product_name=str(result.get('product_name')), 
-                                        product_quantity=int(result.get('product_quantity')), product_price=float(result.get('product_price')))
-        
+        Returns:
+            inventory_pb2.Product: A NULL generated product.
+        """
+        try:
+            print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} Request: All Products')
+            r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+            LOCK.acquire()
+            products = r.keys('*') # Return list of keys that match wildcard (return all keys)
+            
+            # Return empty NULL product if database is empty
+            if len(products) <= 0:
+                return inventory_pb2.Product(product_identifier=-1, product_name="NULL", 
+                                                product_quantity=-1, product_price=float(-1))
+           
+            # Stream out products in database
+            for i in range(len(products)):
+                result = r.hgetall(products[i])
+                yield inventory_pb2.Product(product_identifier=int(products[i]), product_name=str(result.get('product_name')), 
+                                            product_quantity=int(result.get('product_quantity')), product_price=float(result.get('product_price')))
+            LOCK.release()
+            
+        except ConnectionError:
+            print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} Error connecting to redis-server. Maybe the server wasn\'t started?')
+            return inventory_pb2.Product(product_identifier=-2, product_name="NULL", 
+                                            product_quantity=-1, product_price=float(-1))
+    
 
 
 def serve():
+    """
+    Initializes and starts the gRPC server.
+
+    Returns:
+        grpc.Server: The initialized gRPC server.
+    """
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     inventory_pb2_grpc.add_InventoryServiceServicer_to_server(InventoryServiceServicer(), server)
     server.add_insecure_port('[::]:50051')
@@ -59,13 +222,13 @@ if __name__ == '__main__':
     try:
         server = serve()
         server.start()
-        print('Server Started...')
-        print('Kill with keyboard interrupt (Ctrl+C)')
+        print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} Server Started...')
+        print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} Kill with keyboard interrupt (Ctrl+C)')
         server.wait_for_termination()
         
     except KeyboardInterrupt:
         server.stop(0)
         exit(0)
     except:
-        print('An error occurred initiating the server')
+        print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")}An error occurred initiating the server')
         exit(0)
